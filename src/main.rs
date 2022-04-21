9bba49f3baa3be09eb5bd615266ee374e548ac43
pierre <pierre>
2022-04-21 23:05:42 +0200

    many tiles

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 use bevy_asset_loader::{AssetCollection, AssetCollectionApp};
+use bevy_inspector_egui::{Inspectable, RegisterInspectable, WorldInspectorPlugin};
 
 fn main() {
     App::new()
@@ -9,7 +10,10 @@ fn main() {
             color: Color::WHITE,
             brightness: 1.0 / 5.0f32,
         })
+        .add_plugin(WorldInspectorPlugin::new())
         .add_startup_system(setup)
+        .register_inspectable::<Coordinates>()
+        .add_system(apply_coordinate)
         .add_system(animate_light_direction)
         .run();
 }
@@ -20,10 +24,21 @@ struct ModelAssets {
     ground_path_split: Handle<Scene>,
 }
 
+#[derive(Component, Inspectable, Default)]
+struct Coordinates {
+    pub x: i32,
+    pub z: i32,
+}
+
+impl Coordinates {
+    fn new(x: i32, z: i32) -> Self {
+        Self { x, z }
+    }
+}
+
 fn setup(mut commands: Commands, models: Res<ModelAssets>) {
-    commands.spawn_scene(models.ground_path_split.clone());
     commands.spawn_bundle(PerspectiveCameraBundle {
-        transform: Transform::from_xyz(0.7, 0.7, 1.0).looking_at(Vec3::new(0.0, 0.3, 0.0), Vec3::Y),
+        transform: Transform::from_xyz(0.7, 5.0, 1.0).looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::Y),
         ..default()
     });
     const HALF_SIZE: f32 = 1.0;
@@ -43,6 +58,29 @@ fn setup(mut commands: Commands, models: Res<ModelAssets>) {
         },
         ..default()
     });
+
+    for x in -10..10 {
+        for z in -10..10 {
+            commands
+                .spawn_bundle((
+                    Name::from(format!("{x}:{z}")),
+                    Transform::default(),
+                    GlobalTransform::default(),
+                    Coordinates::new(x, z),
+                ))
+                .with_children(|tile| {
+                    tile.spawn_scene(models.ground_path_split.clone());
+                });
+        }
+    }
+}
+
+fn apply_coordinate(mut query: Query<(&mut Transform, &Coordinates), Changed<Coordinates>>) {
+    for (mut transform, coordinates) in query.iter_mut() {
+        transform.translation.x = coordinates.x as f32;
+        transform.translation.y = 0.;
+        transform.translation.z = coordinates.z as f32;
+    }
 }
 
 fn animate_light_direction(
