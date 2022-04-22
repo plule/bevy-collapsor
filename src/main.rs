1324e3ae66d0f3ad39cf356f25f66b3d23042c31
pierre <pierre>
2022-04-22 23:14:44 +0200

    display palette

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 use bevy::prelude::*;
 use bevy_inspector_egui::{Inspectable, RegisterInspectable, WorldInspectorPlugin};
+use bevy_mod_picking::*;
 use rand::Rng;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugin(WorldInspectorPlugin::new())
+        .add_plugins(DefaultPickingPlugins)
         .insert_resource(AmbientLight {
             color: Color::WHITE,
             brightness: 1.0 / 5.0f32,
         })
         .init_resource::<ModelAssets>()
-        .add_plugin(WorldInspectorPlugin::new())
         .add_startup_system(setup)
         .register_inspectable::<Coordinates>()
         .add_system(apply_coordinate)
@@ -23,12 +25,20 @@ struct ModelAssets {
     models: Vec<Handle<Scene>>,
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut models: ResMut<ModelAssets>) {
-    commands.spawn_bundle(PerspectiveCameraBundle {
-        transform: Transform::from_xyz(0.0, 50.0, 0.0)
-            .looking_at(Vec3::new(32.0, 0.0, 32.0), Vec3::Y),
-        ..default()
-    });
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut models: ResMut<ModelAssets>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands
+        .spawn_bundle(PerspectiveCameraBundle {
+            transform: Transform::from_xyz(-10.0, 10.0, 0.0)
+                .looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::Y),
+            ..default()
+        })
+        .insert_bundle(PickingCameraBundle::default());
     const HALF_SIZE: f32 = 1.0;
     commands.spawn_bundle(DirectionalLightBundle {
         directional_light: DirectionalLight {
@@ -61,7 +71,34 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut models: Res
         .map(|path| asset_server.load(path))
         .collect();
 
-    for x in 0..map.width {
+    let pick_mesh = meshes.add(Mesh::from(shape::Cube { size: 1.0 }));
+    let pick_mat = materials.add(StandardMaterial {
+        base_color: Color::rgba(1.0, 1.0, 1.0, 0.1),
+        alpha_mode: AlphaMode::Blend,
+        ..Default::default()
+    });
+
+    for i in 0..map.tile_models.len() {
+        let model = models.models[i].clone();
+        commands
+            .spawn_bundle(PbrBundle {
+                material: pick_mat.clone(),
+                mesh: pick_mesh.clone(),
+                transform: Transform::default(),
+                global_transform: GlobalTransform::default(),
+                ..Default::default()
+            })
+            .insert_bundle(PickableBundle::default())
+            .insert_bundle((
+                Name::from(format!("tile proto {i}")),
+                Coordinates::new(-5, 2 * (i as i32) - 4),
+            ))
+            .with_children(|tile| {
+                tile.spawn_scene(model);
+            });
+    }
+
+    /*for x in 0..map.width {
         for y in 0..map.height {
             let idx = map.tile_at(x, y);
             let model = models.models[idx].clone();
@@ -76,14 +113,14 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut models: Res
                     tile.spawn_scene(model);
                 });
         }
-    }
+    }*/
 }
 
 fn apply_coordinate(mut query: Query<(&mut Transform, &Coordinates), Changed<Coordinates>>) {
     for (mut transform, coordinates) in query.iter_mut() {
         transform.translation.x = coordinates.x as f32;
         transform.translation.y = 0.;
-        transform.translation.z = coordinates.z as f32;
+        transform.translation.z = coordinates.y as f32;
     }
 }
 
@@ -132,13 +169,13 @@ impl Map {
 
 #[derive(Component, Inspectable, Default)]
 struct Coordinates {
-    pub x: usize,
-    pub z: usize,
+    pub x: i32,
+    pub y: i32,
 }
 
 impl Coordinates {
-    fn new(x: usize, z: usize) -> Self {
-        Self { x, z }
+    fn new(x: i32, y: i32) -> Self {
+        Self { x, y }
     }
 }
 
